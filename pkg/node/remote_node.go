b1e6b6f2dcf349b45dc1719bfb62d46644404a78
Adam H <adam@roboadam.com>
2023-10-15 18:55:06 -0400

    stash

--- pkg/node/remote_node.go
+++ pkg/node/remote_node.go
@@ -1,6 +1,8 @@
 package node
 
 import (
+	"net"
+	"tealfs/pkg/raw_net"
 	"tealfs/pkg/tnet"
 )
 
@@ -8,16 +10,19 @@ type RemoteNode struct {
 	NodeId  Id
 	Address string
 	tNet    tnet.TNet
+	conn    net.Conn
 }
 
 func NewRemoteNode(nodeId Id, address string, tNet tnet.TNet) *RemoteNode {
 	return &RemoteNode{NodeId: nodeId, Address: address, tNet: tNet}
 }
 
-func (node *RemoteNode) Connect() {
-	node.tNet.Dial(node.Address)
+func (r *RemoteNode) Connect() {
+	r.conn = r.tNet.Dial(r.Address)
+	_ = raw_net.Int8To(r.conn, 1)
+	_ = raw_net.StringTo(r.conn, r.NodeId.String())
 }
 
-func (node *RemoteNode) Disconnect() {
-	node.tNet.Close()
+func (r *RemoteNode) Disconnect() {
+	r.tNet.Close()
 }
--- pkg/raw_net/raw_net.go
+++ pkg/raw_net/raw_net.go
@@ -6,13 +6,24 @@ import (
 )
 
 func IntFrom(conn net.Conn) (uint32, error) {
-	buf := make([]byte, 4) // 4 bytes for a 32-bit integer
+	buf := make([]byte, 4)
 	_, err := conn.Read(buf)
 	if err != nil {
 		return 0, err
 	}
 
-	// Convert the received bytes to an integer (big endian)
 	intValue := binary.BigEndian.Uint32(buf)
 	return intValue, nil
+}
+
+func Int8To(conn net.Conn, value int8) error {
+	networkValue := []byte{byte(value)}
+	_, err := conn.Write(networkValue)
+	return err
+}
+
+func StringTo(conn net.Conn, value string) error {
+	networkValue := []byte(value)
+	_, err := conn.Write(networkValue)
+	return err
 }
