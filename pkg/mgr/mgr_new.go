62a31a447f857ea4983d8d0918efdd2753a4ae68
Adam H <Roboadam@users.noreply.github.com>
2024-02-08 22:02:56 -0500

    stash

--- pkg/mgr/mgr_new.go
+++ pkg/mgr/mgr_new.go
@@ -1,8 +1,36 @@
 package mgr
 
 type MgrNew struct {
-	uiCommandsIn chan UiCommand
+	uiCommands  <-chan UiCommand
+	connections <-chan IncomingConnection
 }
 
 type UiCommand struct {
+}
+
+type IncomingConnection struct {
+}
+
+type MutationRequest struct {
+}
+
+func (m *MgrNew) Start() {
+	for {
+		var mutationRequest MutationRequest
+
+		select {
+		case cmd := <-m.uiCommands:
+			mutationRequest = m.uiCommandToMutationRequest(cmd)
+		case conn := <-m.connections:
+			mutationRequest = m.incomingConnectionToMutationRequest(conn)
+		}
+	}
+}
+
+func (m *MgrNew) uiCommandToMutationRequest(_ UiCommand) MutationRequest {
+	return MutationRequest{}
+}
+
+func (m *MgrNew) incomingConnectionToMutationRequest(_ IncomingConnection) MutationRequest {
+	return MutationRequest{}
 }
