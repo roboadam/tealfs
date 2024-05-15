d5d6aa40be0139b52decfccdc3b813efffcc9105
Adam H <Roboadam@users.noreply.github.com>
2024-05-14 21:57:35 -0400

    stash

--- pkg/proto/common.go
+++ pkg/proto/common.go
@@ -1,6 +1,7 @@
 package proto
 
 import (
+	"bytes"
 	"encoding/binary"
 	"tealfs/pkg/store"
 )
@@ -16,6 +17,18 @@ func StringToBytes(value string) []byte {
 	length := uint32(len(rawString))
 	rawLength := IntToBytes(length)
 	return append(rawLength, rawString...)
+}
+
+func BytesFromBytes(data []byte) ([]byte, []byte) {
+	length, remainder := IntFromBytes(data)
+	result := remainder[:length]
+	return result, remainder[length:]
+}
+
+func BytesToBytes(value []byte) []byte {
+	length := uint32(len(value))
+	rawLength := IntToBytes(length)
+	return append(rawLength, value...)
 }
 
 func IntFromBytes(data []byte) (uint32, []byte) {
@@ -44,23 +57,30 @@ func BoolFromBytes(value []byte) (bool, []byte) {
 }
 
 func BlockToBytes(value store.Block) []byte {
-	result := make([]byte, 0)
 	id := StringToBytes(string(value.Id))
-	data := value.Data
-	hash := value.Hash.Value
+	data := BytesToBytes(value.Data)
+	hash := BytesToBytes(value.Hash.Value)
+	numChildren := IntToBytes(uint32(len(value.Children)))
 
-	result = append(result, id...)
-	result = append(result, data...)
-	result = append(result, hash...)
+	result := bytes.Join([][]byte{id, data, hash, numChildren}, []byte{})
 	for _, child := range value.Children {
 		result = append(result, StringToBytes(string(child))...)
 	}
+
 	return result
 }
 
 func BlockFromBytes(value []byte) (store.Block, []byte) {
 	id, remainder := StringFromBytes(value)
-	data := remainder
+	data, remainder := BytesFromBytes(remainder)
+	hash, remainder := BytesFromBytes(remainder)
+	//todo: get number of children
+	var children []store.Block
+	for {
+		if len(remainder) == 0 {
+			return store.Block{}, rem
+		}
+	}
 }
 
 func AddType(id uint8, data []byte) []byte {
