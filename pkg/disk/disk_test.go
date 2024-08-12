bee09614f76eb6d96601b6c382e7685230df26a1
Adam H <Roboadam@users.noreply.github.com>
2024-08-11 21:55:50 -0400

    stash

--- pkg/disk/disk_test.go
+++ pkg/disk/disk_test.go
@@ -16,6 +16,8 @@ package disk_test
 
 import (
 	"bytes"
+	"encoding/hex"
+	"path/filepath"
 	"tealfs/pkg/disk"
 	"tealfs/pkg/hash"
 	"tealfs/pkg/model"
@@ -33,6 +35,7 @@ func TestWriteData(t *testing.T) {
 	blockId := model.NewBlockId()
 	data := []byte{0, 1, 2, 3, 4, 5}
 	h := hash.ForData(data)
+	expectedPath := filepath.Join(path.String(), hex.EncodeToString(h.Value))
 	_ = disk.New(path, id, mgrDiskWrites, mgrDiskReads, diskMgrWrites, diskMgrReads)
 	mgrDiskWrites <- model.Block{
 		Id:   blockId,
@@ -46,7 +49,49 @@ func TestWriteData(t *testing.T) {
 	if !bytes.Equal(f.WrittenData, data) {
 		t.Error("Written data is wrong")
 	}
-	if f.WritePath != path.String() {
+	if f.WritePath != expectedPath {
 		t.Error("Written path is wrong")
 	}
+}
+
+func TestReadData(t *testing.T) {
+	f := disk.MockFileOps{}
+	path := disk.NewPath("/some/fake/path", &f)
+	id := model.NewNodeId()
+	mgrDiskWrites := make(chan model.Block)
+	mgrDiskReads := make(chan model.ReadRequest)
+	diskMgrWrites := make(chan model.WriteResult)
+	diskMgrReads := make(chan model.ReadResult)
+	blockId := model.NewBlockId()
+	data := []byte{0, 1, 2, 3, 4, 5}
+	h := hash.ForData(data)
+	expectedPath := filepath.Join(path.String(), hex.EncodeToString(h.Value))
+	_ = disk.New(path, id, mgrDiskWrites, mgrDiskReads, diskMgrWrites, diskMgrReads)
+	mgrDiskWrites <- model.Block{
+		Id:   blockId,
+		Data: data,
+		Hash: h,
+	}
+	result := <-diskMgrWrites
+	if !result.Ok {
+		t.Error("Bad write result")
+	}
+	if !bytes.Equal(f.WrittenData, data) {
+		t.Error("Written data is wrong")
+	}
+	if f.WritePath != expectedPath {
+		t.Error("Written path is wrong")
+	}
+}
+
+func NewDiskService() (disk.MockFileOps, disk.Path, model.NodeId, chan model.Block, chan model.ReadRequest, chan model.WriteResult, chan model.ReadResult, disk.Disk ) {
+	f := disk.MockFileOps{}
+	path := disk.NewPath("/some/fake/path", &f)
+	id := model.NewNodeId()
+	mgrDiskWrites := make(chan model.Block)
+	mgrDiskReads := make(chan model.ReadRequest)
+	diskMgrWrites := make(chan model.WriteResult)
+	diskMgrReads := make(chan model.ReadResult)
+	d := disk.New(path, id, mgrDiskWrites, mgrDiskReads, diskMgrWrites, diskMgrReads)
+	return f, path, id, mgrDiskWrites, mgrDiskReads, diskMgrWrites, diskMgrReads, d
 }
