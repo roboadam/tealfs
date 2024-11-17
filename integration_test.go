649fb6128fad180989004f3b2961e035ffc7c52d
Adam H <Roboadam@users.noreply.github.com>
2024-11-16 21:33:00 -0500

    stash

--- integration_test.go
+++ integration_test.go
@@ -29,25 +29,16 @@ func TestOneNodeCluster(t *testing.T) {
 	nodeAddress := "localhost:8082"
 	storagePath := "tmp"
 	webdavUrl := "http://" + webdavAddress
-	client := http.Client{}
 	os.Mkdir(storagePath, 0755)
 	defer os.RemoveAll(storagePath)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	go startTealFs(storagePath, webdavAddress, uiAddress, nodeAddress, ctx)
-	time.Sleep(time.Second) // TODO have this wait on a message if possible
+	time.Sleep(time.Second)
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodPut, webdavUrl+"/text.txt", bytes.NewBufferString("test content"))
-	if err != nil {
-		t.Error("error creating request", err)
-		return
-	}
-	req.Header.Set("Content-Type", "text/plain")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Error("error executing request", err)
+	resp, ok := putFile(ctx, webdavUrl+"/test.txt", "test content", t)
+	if !ok {
 		return
 	}
 	defer resp.Body.Close()
@@ -56,4 +47,21 @@ func TestOneNodeCluster(t *testing.T) {
 		t.Error("error response", resp.Status)
 		return
 	}
+}
+
+func putFile(ctx context.Context, url string, contents string, t *testing.T) (*http.Response, bool) {
+	client := http.Client{}
+	req, err := http.NewRequestWithContext(ctx, http.MethodPut, url, bytes.NewBufferString(contents))
+	if err != nil {
+		t.Error("error creating request", err)
+		return nil, false
+	}
+	req.Header.Set("Content-Type", "text/plain")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Error("error executing request", err)
+		return nil, false
+	}
+	return resp, true
 }
