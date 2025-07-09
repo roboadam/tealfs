a6428d704e6fd2413325687236cc9ef1a7de395d
Adam H <adam@roboadam.com>
2025-07-08 22:11:55 -0400

    stash

---
+++ pkg/blocksaver/blocksaver.go
@@ -0,0 +1,34 @@
+// Copyright (C) 2025 Adam Hess
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+package blocksaver
+
+import (
+	"context"
+	"tealfs/pkg/model"
+)
+
+type BlockSaver struct {
+	Req chan model.PutBlockReq
+}
+
+func (bs *BlockSaver) Start(ctx context.Context) {
+	for {
+		select {
+		case <-bs.Req:
+		case <-ctx.Done():
+			return
+		}
+	}
+}
