b60e8039a81abd41b444658bf41820bdc603f719
Adam H <adam@roboadam.com>
2025-07-13 21:28:13 -0400

    stash

---
+++ pkg/blocksaver/local_block_save_response_test.go
@@ -0,0 +1,77 @@
+// Copyright (C) 2025 Adam Hess
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+package blocksaver
+
+import (
+	"context"
+	"tealfs/pkg/disk"
+	"tealfs/pkg/model"
+	"tealfs/pkg/set"
+	"testing"
+
+	"github.com/google/uuid"
+)
+
+func TestLocalBlockSaveResponse(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	nodeId := model.NewNodeId()
+
+	disk1 := mockDisk(nodeId, ctx)
+	disk2 := mockDisk(nodeId, ctx)
+	disks := set.NewSet[disk.Disk]()
+	disks.Add(*disk1)
+	disks.Add(*disk2)
+
+	resp := make(chan SaveToDiskResp)
+
+	lbsr := LocalBlockSaveResponses{
+		Disks:          &disks,
+		WriteResponses: resp,
+	}
+
+	go lbsr.Start(ctx)
+
+	putBlockId := model.PutBlockId(uuid.NewString())
+	disk1.OutWrites <- model.NewWriteResultOk(
+		model.DiskPointer{
+			NodeId:   nodeId,
+			Disk:     disk1.Id(),
+			FileName: uuid.NewString(),
+		},
+		nodeId,
+		putBlockId,
+	)
+
+	wr := <-resp
+	if wr.Resp.Id != putBlockId {
+		t.Error("Unknown put block id")
+		return
+	}
+
+	putBlockId2 := model.PutBlockId(uuid.NewString())
+	disk2.OutWrites <- model.NewWriteResultErr(
+		"some error happened",
+		nodeId,
+		putBlockId2,
+	)
+
+	wr = <-resp
+	if wr.Resp.Id != putBlockId2 {
+		t.Error("Unknown put block id")
+		return
+	}
+}
