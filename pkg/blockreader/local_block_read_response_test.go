74df5e2b3b25e4a3148de636a0cc4d8f57b4fcda
Adam H <94359+roboadam@users.noreply.github.com>
2025-07-23 21:09:59 -0400

    ok

--- pkg/blockreader/local_block_read_response_test.go
+++ pkg/blockreader/local_block_read_response_test.go
@@ -18,9 +18,11 @@ func TestLocalBlockReadResponse(t *testing.T) {
 	sends := make(chan model.MgrConnsSend)
 	nodeConnMap := model.NewNodeConnectionMapper()
 	localNodeId := model.NewNodeId()
-	// remoteNodeId := model.NewNodeId()
+	remoteNodeId := model.NewNodeId()
 	blockId1 := model.NewBlockId()
+	blockId2 := model.NewBlockId()
 	reqId1 := model.GetBlockId(uuid.NewString())
+	reqId2 := model.GetBlockId(uuid.NewString())
 	lbrr := LocalBlockReadResponses{
 		InDisks:            inDisks,
 		LocalReadResponses: localReadResponses,
@@ -46,9 +48,32 @@ func TestLocalBlockReadResponse(t *testing.T) {
 		ReqId:   reqId1,
 	}
 
-	resp := <- localReadResponses 
+	resp := <-localReadResponses
 	if resp.Resp.Id != reqId1 {
 		t.Error("invalid request id")
+		return
+	}
+
+	disk2.InReads <- model.ReadRequest{
+		Caller: remoteNodeId,
+		Ptrs: []model.DiskPointer{{
+			NodeId:   localNodeId,
+			Disk:     disk2.Id(),
+			FileName: string(blockId2),
+		}},
+		BlockId: blockId2,
+		ReqId:   reqId2,
+	}
+
+	nodeConnMap.SetAll(model.ConnId(1), "someAddress:123", remoteNodeId)
+
+	resp2 := <-sends
+	payload := resp2.Payload
+	if gfdr, ok := payload.(*GetFromDiskResp); !ok {
+		t.Error("sent wrong type")
+		return
+	} else if gfdr.Resp.Id != reqId2 {
+		t.Error("sent wrong message")
 		return
 	}
 }
