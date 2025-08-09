5e8e46e130fd36089324cedb057cc45509efb146
Adam H <94359+roboadam@users.noreply.github.com>
2025-08-08 20:33:21 -0400

    stash

--- pkg/model/nodeConnectionMaper.go
+++ pkg/model/nodeConnectionMaper.go
@@ -146,28 +146,6 @@ func NodeConnectionMapperUnmarshal(data []byte) (*NodeConnectionMapper, error) {
 	return &result, nil
 }
 
-// func (n *NodeConnectionMapper) AddressesAndNodes() set.Set[struct {
-// 	Address string
-// 	NodeId  NodeId
-// }] {
-// 	n.mux.RLock()
-// 	defer n.mux.RUnlock()
-// 	result := set.NewSet[struct {
-// 		Address string
-// 		NodeId  NodeId
-// 	}]()
-// 	for _, values := range n.addressNodeMap.AllValues() {
-// 		result.Add(struct {
-// 			Address string
-// 			NodeId  NodeId
-// 		}{
-// 			Address: values.K,
-// 			NodeId:  values.J,
-// 		})
-// 	}
-// 	return result
-// }
-
 func (n *NodeConnectionMapper) SetAll(conn ConnId, address string, node NodeId) {
 	n.mux.Lock()
 	defer n.mux.Unlock()
