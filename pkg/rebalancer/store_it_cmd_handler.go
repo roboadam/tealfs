b97de17e7edec3b97c354dd3a11fa1090e9ae75c
Adam H <94359+roboadam@users.noreply.github.com>
2025-10-25 23:01:09 -0400

    stash

--- pkg/rebalancer/store_it_cmd_handler.go
+++ pkg/rebalancer/store_it_cmd_handler.go
@@ -16,32 +16,71 @@ package rebalancer
 
 import (
 	"context"
-	"tealfs/pkg/disk"
+	"tealfs/pkg/model"
 	"tealfs/pkg/set"
 )
 
 type StoreItCmdHandler struct {
-	InStoreItCmd <-chan StoreItCmd
+	InStoreItCmd  <-chan StoreItCmd
+	InStoreItResp <-chan StoreItResp
 	OutStoreItReq chan<- StoreItReq
 
-	Disks *set.Set[disk.Disk]
+	AllDiskIds  *set.Set[model.AddDiskReq]
+	bookKeeping map[BalanceReqId]map[model.BlockId]*set.Set[model.AddDiskReq]
 }
 
 func (s *StoreItCmdHandler) Start(ctx context.Context) {
+	s.bookKeeping = make(map[BalanceReqId]map[model.BlockId]*set.Set[model.AddDiskReq])
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		case cmd := <-s.InStoreItCmd:
 			s.handleStoreItCmd(cmd)
+		case resp := <-s.InStoreItResp:
+			s.handleStoreItResp(resp)
 		}
 	}
 }
 
+func (s *StoreItCmdHandler) handleStoreItResp(resp StoreItResp) {
+	if !resp.Ok {
+		s.sendNextStoreItReq(resp.Req)
+	}
+
+}
+
 func (s *StoreItCmdHandler) handleStoreItCmd(cmd StoreItCmd) {
-	s.OutStoreItReq <- StoreItReq{
-		Req:    cmd,
-		NodeId: cmd.DestNodeId,
-		DiskId: cmd.DestDiskId,
+	s.initBookKeeping(cmd)
+	s.bookKeeping[cmd.BalanceReqId][cmd.DestBlockId] = s.AllDiskIds
+	s.sendNextStoreItReq(cmd)
+}
+
+func (s *StoreItCmdHandler) sendNextStoreItReq(cmd StoreItCmd) {
+	dests := s.bookKeeping[cmd.BalanceReqId][cmd.DestBlockId]
+	for {
+		next, remainder, ok := dests.Pop()
+		s.bookKeeping[cmd.BalanceReqId][cmd.DestBlockId] = remainder
+
+		if !ok {
+			break
+		}
+		if next.NodeId == cmd.Caller {
+			continue
+		}
+
+		s.OutStoreItReq <- StoreItReq{
+			Req:    cmd,
+			NodeId: next.NodeId,
+			DiskId: next.DiskId,
+		}
+		break
+	}
+}
+
+func (s *StoreItCmdHandler) initBookKeeping(cmd StoreItCmd) {
+	_, ok := s.bookKeeping[cmd.BalanceReqId]
+	if !ok {
+		s.bookKeeping[cmd.BalanceReqId] = make(map[model.BlockId]*set.Set[model.AddDiskReq])
 	}
 }
