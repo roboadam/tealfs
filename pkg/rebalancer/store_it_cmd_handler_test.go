75f600d4058d2320a5960403bbe0c15ddd536097
Adam H <94359+roboadam@users.noreply.github.com>
2025-11-08 21:51:06 -0500

    stash

--- pkg/rebalancer/store_it_cmd_handler_test.go
+++ pkg/rebalancer/store_it_cmd_handler_test.go
@@ -100,3 +100,70 @@ func TestStoreItCmd(t *testing.T) {
 		t.Error("unexpected disks tried")
 	}
 }
+
+func TestStoreItCmdSuccess(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	inStoreItCmd := make(chan rebalancer.StoreItCmd)
+	inStoreItResp := make(chan rebalancer.StoreItResp)
+	outStoreItReq := make(chan rebalancer.StoreItReq)
+	outExistsResp := make(chan rebalancer.ExistsResp)
+
+	localDisks := set.NewSet[disk.Disk]()
+	fileOps := disk.MockFileOps{}
+	path := disk.NewPath("", &fileOps)
+	localDisks.Add(disk.New(path, "localNode", "localDisk1", ctx))
+	localDisks.Add(disk.New(path, "localNode", "localDisk2", ctx))
+
+	allDiskIds := set.NewSet[model.AddDiskReq]()
+	allDiskIds.Add(model.AddDiskReq{DiskId: "localDisk1", Path: "", NodeId: "localNode"})
+	allDiskIds.Add(model.AddDiskReq{DiskId: "localDisk2", Path: "", NodeId: "localNode"})
+	allDiskIds.Add(model.AddDiskReq{DiskId: "remoteDisk1", Path: "", NodeId: "remoteNode"})
+	allDiskIds.Add(model.AddDiskReq{DiskId: "remoteDisk2", Path: "", NodeId: "remoteNode"})
+
+	handler := rebalancer.StoreItCmdHandler{
+		InStoreItCmd:  inStoreItCmd,
+		InStoreItResp: inStoreItResp,
+		OutStoreItReq: outStoreItReq,
+		OutExistsResp: outExistsResp,
+		AllDiskIds:    &allDiskIds,
+		LocalDisks:    &localDisks,
+	}
+
+	go handler.Start(ctx)
+
+	cmd := rebalancer.StoreItCmd{
+		Caller:       "mainNodeId",
+		BalanceReqId: "balanceReqId",
+		StoreItId:    "storeItId",
+		DestNodeId:   "localNode",
+		DestDiskId:   "localDisk1",
+		DestBlockId:  "blockId",
+		ExistsReq: rebalancer.ExistsReq{
+			Caller:       "mainNodeId",
+			BalanceReqId: "balanceReqId",
+			ExistsId:     "existsId",
+			DestNodeId:   "localNode",
+			DestDiskId:   "localDisk1",
+			DestBlockId:  "blockId",
+		},
+	}
+
+	inStoreItCmd <- cmd
+
+	disksTried := set.NewSet[model.DiskId]()
+	req := <-outStoreItReq
+	disksTried.Add(req.DiskId)
+
+	block := model.Block{
+		Data: []byte("blockData"),
+		Id:   "blockId",
+	}
+	inStoreItResp <- rebalancer.StoreItResp{Req: req, Ok: true, Block: block}
+
+	existsResp := <-outExistsResp
+	if existsResp.Req.ExistsId != "existsId" {
+		t.Error("invalid exists id")
+	}
+}
